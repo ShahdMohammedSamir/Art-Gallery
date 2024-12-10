ec7458397911b98ce99fb44d597262fe531ac3db
shahd mohammed <shhdmm932@gmail.com>
2024-12-11 00:21:40 +0200

    Add files via upload

---
+++ Backend/Helpers/CloudinarySettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtGalleryFinal.Helpers
+{
+    public class CloudinarySettings
+
+    { 
+        public string CloudName { get; set; }
+    public string ApiKey { get; set; }
+    public string ApiSecret { get; set; }
+}
+}
