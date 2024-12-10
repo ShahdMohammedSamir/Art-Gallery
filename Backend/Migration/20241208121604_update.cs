b4589b957a9c01fe1fa49d066c64c5135c0bdfa6
shahd mohammed <shhdmm932@gmail.com>
2024-12-11 01:54:58 +0200

    Migration

---
+++ Backend/Migration/20241208121604_update.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtGalleryFinal.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Amount",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Price",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Orders");
+        }
+    }
+}
