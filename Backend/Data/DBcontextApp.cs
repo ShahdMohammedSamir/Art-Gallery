ff15d39466eb86989186ce55cc36e0f8f381397f
shahd mohammed <shhdmm932@gmail.com>
2024-12-11 00:19:36 +0200

    Add files via upload

---
+++ Backend/Data/DBcontextApp.cs
@@ -0,0 +1,34 @@
+﻿using ArtGalleryFinal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtGalleryFinal.Data
+{
+    public class DBcontextApp:DbContext
+    {
+        public DBcontextApp(DbContextOptions<DBcontextApp> options) : base(options) { }
+
+        public DbSet<Products> Products { get; set; }
+        public DbSet<Users> Users { get; set; }
+
+        public DbSet<Orders> Orders { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+            modelBuilder.Entity<Orders>()
+                .HasOne(o => o.User)
+                .WithMany(u => u.Orders)
+                .HasForeignKey(o => o.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+           
+
+            modelBuilder.Entity<Orders>()
+                .HasOne(o => o.Product)
+                .WithMany(p => p.Orders)
+                .HasForeignKey(o => o.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
